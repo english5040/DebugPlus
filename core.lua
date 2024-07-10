c4ad5c5578498119d8c5aee013fbdc498d6cdc56
WilsontheWolf <33164598+WilsontheWolf@users.noreply.github.com>
2024-07-09 21:29:58 -0600

    feat: basics of command handler

--- core.lua
+++ core.lua
@@ -63,7 +63,24 @@ local function getEnhancements()
     return enhancements
 end
 
-function global.consoleHandleKey(controller, key, dt)
+local function runCommand() 
+    if inputText == "" then return end
+    handleLog({1, 0, 1}, "> " .. inputText)
+
+    local args = {}
+    for w in string.gmatch(inputText, "%a+") do
+        table.insert(args, w)
+    end
+    local cmd = table.remove(args, 1)
+    log("Command:", cmd)
+    log("args:", inspect(args))
+
+
+    -- Cleanup
+    inputText = ""
+end
+
+function global.consoleHandleKey(controller, key)
     if not consoleOpen then
         if key == '/' then
             if love.keyboard.isDown('lshift') then
@@ -75,7 +92,6 @@ function global.consoleHandleKey(controller, key, dt)
         return true
     end
 
-    log("Key:", key)
     if key == "escape" then
         consoleOpen = false
         inputText = ""
@@ -93,7 +109,11 @@ function global.consoleHandleKey(controller, key, dt)
     end
 
     if key == "return" then
-
+        if love.keyboard.isDown('lshift') then
+            inputText = inputText .. "\n"
+        else
+            runCommand()
+        end
     end
 
 end
@@ -106,7 +126,6 @@ function love.textinput(t)
     if not consoleOpen then
         return
     end
-    log("text:", t)
     inputText = inputText .. t
 end
 
@@ -298,7 +317,7 @@ local function calcHeight(text, width)
     local rw, lines = font:getWrap(text, width)
     local lineHeight = font:getHeight()
 
-    return #lines * lineHeight, rw
+    return #lines * lineHeight, rw, lineHeight
 end
 
 global.registerLogHandler = function()
@@ -333,10 +352,10 @@ global.doConsoleRender = function()
     love.graphics.setColor(0, 0, 0, .5)
     if consoleOpen then
         bottom = bottom - padding * 2
-        local lineHeight, realWidth = calcHeight(inputText, lineWidth)
+        local lineHeight, realWidth, singleLineHeight = calcHeight(inputText, lineWidth)
         love.graphics.rectangle("fill", padding, bottom - lineHeight + padding, lineWidth, lineHeight + padding * 2)
         love.graphics.setColor(1, 1, 1, 1)
-        love.graphics.printf(inputText, padding * 2, bottom, lineWidth - padding * 2)
+        love.graphics.printf(inputText, padding * 2, bottom - lineHeight + singleLineHeight, lineWidth - padding * 2)
 
         bottom = bottom - lineHeight - padding * 2
     end
